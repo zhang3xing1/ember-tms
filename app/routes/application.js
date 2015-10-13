157a29416438a4e2335394d9137c3bed693e6e31
XingZhang <xzhang@coupang.com>
2015-10-13 15:26:51 +0800

    add map style control of map.

--- app/routes/application.js
+++ app/routes/application.js
@@ -8,7 +8,21 @@ export default Ember.Route.extend({
                     lat: 37.43890643,
                     lng: 127.1235116
                 },
-                zoom: 16
+                zoom: 16,
+                mapTypeControl: true,
+                mapTypeControlOptions: {
+                    style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+                    position: google.maps.ControlPosition.TOP_CENTER
+                },
+                zoomControl: true,
+                // zoomControlOptions: {
+                //     position: google.maps.ControlPosition.LEFT_CENTER
+                // },
+                scaleControl: true,
+                // streetViewControl: true,
+                // streetViewControlOptions: {
+                //     position: google.maps.ControlPosition.LEFT_CENTER
+                // }
             }),
 
             hi3: 'foobar',
